4787bab6efb1046d51ad3c4fe3243138e022c179
Tom Kmec <tom.kmec@gmail.com>
2021-02-08 08:51:32 +0100

    BE skeleton scaffolded by start.spring.io, FE by create-react-app

---
+++ backend/src/main/kotlin/net/tomkmec/azeti/AzetiApplication.kt
@@ -0,0 +1,11 @@
+package net.tomkmec.azeti
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AzetiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AzetiApplication>(*args)
+}
---
+++ backend/src/test/kotlin/net/tomkmec/azeti/AzetiApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.tomkmec.azeti
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AzetiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
